58c0b4f7db80110bde6d5701658848bfc479e3f1
daniel141292 <34457005+daniel141292@users.noreply.github.com>
2025-06-20 17:01:28 -0600

    se realiza la conexion a la base de datos mongodb

---
+++ app/config/conexion.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const config = require('./configuracion')
+
+module.exports={
+    connecction : null,
+    connect : ()=>{
+        if(this.connecction) return this.connecction
+        return mongoose.connect(config.DB)
+        .then(conn =>{
+            this.connecction = conn
+            console.log('la conexion se realizo de manera correcta');
+                
+        })
+        .catch(e =>{console.log('error en la conexion ',e)})
+    }
+}
